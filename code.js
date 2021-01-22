4b1a615d79ac1aaf20573bfc743ffcfc8161c5d3
Johan Abrahamsson <71386129+Johan-Abrhamsson@users.noreply.github.com>
2021-01-22 16:23:22 +0100

    Start of set

---
+++ code.js
@@ -0,0 +1,16 @@
+var players = 0
+var captianPlayer = 0
+let stack1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+let stack2 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+let stack3 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+let stack4 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+let stack5 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+let stack6 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+let stack7 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+let stack8 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+let stack9 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+let eng = [stack1, stack2, stack3, stack4, stack5, stack6, stack7, stack8, stack9]
+
+
+
+console.log(eng[0])
