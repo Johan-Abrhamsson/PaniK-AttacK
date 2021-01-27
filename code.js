6e2f927619449fc6a71fe8b681743cad4725c85d
Johan Abrahamsson <71386129+Johan-Abrhamsson@users.noreply.github.com>
2021-01-27 11:20:05 +0100

    Picture start

--- code.js
+++ code.js
@@ -11,6 +11,48 @@ let stack8 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 let stack9 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 let eng = [stack1, stack2, stack3, stack4, stack5, stack6, stack7, stack8, stack9]
 
+function addNumber(a, b){
+    return a+b;
+}
 
+let myFunction = addNumbers;
+console.log(typeof myFunction);
+console.log(myFunction)
+
+console.log(myFunktion(3,3));
 
 console.log(eng[0])
+
+function runfunction(functionToRun){
+    console.log(typeof functionToRun);
+    console.log(functionToRun)
+}
+runfunction(addNumbers);
+
+let myButton = dokument.querySelector(".myButton");
+console.log(myBotton);
+console.log(typeof myButton);
+
+myButton.addEventListener("click", () => {
+    console.log("hello world");
+});
+
+let myFunction = function(a, b){
+    console.log(a+b);
+}
+
+
+
+function action(a, p){
+    if (a=="idf"){
+        runfunction(idf);
+    }
+    else if (a=="dpl"){
+        runfunction(dpl);
+    }
+    else if (a=="dtc"){
+        runfunction("dtc");
+    }
+}
+
+
